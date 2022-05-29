a7f63819ffd15c9be3403d93778c4e0c52db7748
Legolas <geran.vanatta@gmail.com>
2022-05-29 14:00:33 -0500

    finished dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,10 +5,9 @@ const withAuth = require('../utils/auth');
 
 // get all posts for dashboard
 router.get('/', withAuth, (req, res) => {
-    console.log(req.session);
-    console.log('======================');
-    Post.findAll({
+  Post.findAll({
       where: {
+        // use the ID from the session
         user_id: req.session.user_id
       },
       attributes: [
@@ -16,7 +15,6 @@ router.get('/', withAuth, (req, res) => {
         'post_url',
         'title',
         'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
       ],
       include: [
         {
@@ -34,6 +32,7 @@ router.get('/', withAuth, (req, res) => {
       ]
     })
       .then(dbPostData => {
+        // serialize data before passing to template
         const posts = dbPostData.map(post => post.get({ plain: true }));
         res.render('dashboard', { posts, loggedIn: true });
       })
@@ -41,47 +40,49 @@ router.get('/', withAuth, (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
-  
-  router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findByPk(req.params.id, {
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    })
-      .then(dbPostData => {
-        if (dbPostData) {
-          const post = dbPostData.get({ plain: true });
-          
-          res.render('edit-post', {
-            post,
-            loggedIn: true
-          });
-        } else {
-          res.status(404).end();
-        }
-      })
-      .catch(err => {
-        res.status(500).json(err);
+});
+
+router.get('/edit/:id', withAuth, (req, res) => {
+Post.findOne({
+  where: {
+    id: req.params.id
+  },
+  attributes: [
+    'id',
+    'post_url',
+    'title',
+    'created_at',
+  ],
+  include: [
+    {
+      model: Comment,
+      attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+      include: {
+        model: User,
+        attributes: ['username']
+      }
+    },
+    {
+      model: User,
+      attributes: ['username']
+    }
+  ]
+})
+  .then(dbPostData => {
+    if (dbPostData) {
+      const post = dbPostData.get({ plain: true });
+      
+      res.render('edit-post', {
+        post,
+        loggedIn: true
       });
+    } else {
+      res.status(404).end();
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err);
   });
-  
-  module.exports = router;
+});
+
+module.exports = router;
